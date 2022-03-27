e575a25f828ab22afe96e8aa4627286b71609478
Matt Daly <matthew.daly@visma.com>
2022-03-27 02:05:12 +0100

    Housekeeping

--- src/utilities/defaultProps.ts
+++ src/utilities/defaultProps.ts
@@ -3,9 +3,7 @@ import * as React from 'react';
 export function defaultProps<T, P = {}>(
     element: React.FunctionComponent<P> | React.ComponentClass<P> | string,
     props: Partial<P>
-): React.ForwardRefExoticComponent<
-    React.PropsWithoutRef<P> & React.RefAttributes<T>
-> {
+): React.ForwardRefExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<T>> {
     return React.forwardRef<T, P>((externalProps, externalRef) => {
         const forwardProps = {
             ...externalProps,
